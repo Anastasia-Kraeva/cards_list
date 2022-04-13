218e7d2ea9f9ced459a66a6050acac840fcef7c3
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2022-04-13 18:04:13 +0300

    created initial app structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostsList from './components/PostsList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Техническое задание фронтенд разработчика</h1>
+      <h2>Подзаголовок, чтобы не пропускать заголовок "h2", но и не вписывать его в карточки</h2>
+      <PostsList/>
+    </div>
+  );
+}
+
+export default App;
